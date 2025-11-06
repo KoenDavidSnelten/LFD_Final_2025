01d745dee4d9cce95e31584816cf3a87c823c841
Koen Snelten <koensnelten@gmail.com>
2025-11-06 19:30:42 +0100

    U

---
+++ data/pre_processor.py
@@ -0,0 +1,33 @@
+
+def emoij_to_text():
+    return
+
+
+def lower_case():
+    return 
+
+def remove_user():
+    return
+
+def remove_url():
+    return
+
+def remove_hastag():
+    return
+
+def remove_extra_whitespace():
+    return
+
+def remove_html():
+    return
+
+def open_file():
+    return
+
+
+def main():
+    print()
+    return
+
+if __name__ == "__main__":
+    main()
